a3d9719babccf5cfd7c38156eaee6a644bc6b8c3
chasehaye <chasehayejoc@gmail.com>
2025-01-28 00:02:07 -0700

    fixed register

--- src/AuthPage/AuthForms/Resgister.jsx
+++ src/AuthPage/AuthForms/Resgister.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import * as userService from '../../utilities/user-service';
 import { UserContext } from '../../UserProvider';
 import { useNavigate } from 'react-router-dom';
